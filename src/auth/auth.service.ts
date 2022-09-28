fc4b67e00898a8ce2fe6cccfdd510f6dfcb94068
devguusta <gustavodev60@gmail.com>
2022-09-28 17:48:25 -0300

    chore: create signup function in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, ForbiddenException, HttpException, Injectable } from "@nestjs/common";
 import { argon2d } from "argon2";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class  AuthService{
@@ -10,17 +11,46 @@ export class  AuthService{
 
   
  async signup(dto: AuthDto){
-    const hash = await argon.hash(dto.password);  
-    const user = await this.prisma.user.create({
-        data: {
-            email: dto.email,
-            hash,
-        },
+
+    try {
+
+        const oldUser = await this.prisma.user.findFirst(
+            {
+                where: {
+                    email: dto.email
+                }
+            }
+        );
+        console.log(oldUser);
+        if(oldUser){
+            throw new BadRequestException({
+                message: "User already exists"
+            })
+        }
+
+
+        const hash = await argon.hash(dto.password);  
+
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+            
+        });
         
-    });
-    
-    delete user.hash;
-    return user;
+        delete user.hash;
+        return user;
+    } catch (error) {
+        if(error instanceof PrismaClientKnownRequestError){
+            if(error.code === "P2002"){
+                throw new ForbiddenException('Credentials taken');
+            }
+        }
+        throw error;
+    }
+   
     }
 
     signin(){
