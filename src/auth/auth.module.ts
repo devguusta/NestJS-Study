6d470995eae252ea51e4a7c4d967904aaea2f945
devguusta <gustavodev60@gmail.com>
2022-10-01 12:27:42 -0300

    chore: create autw jwt signin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
@@ -6,7 +7,9 @@ import { AuthService } from "./auth.service";
 
 
 @Module({
-    
+    imports: [JwtModule.register({
+        
+    })],
     controllers: [AuthController],
     providers: [AuthService]
 })
